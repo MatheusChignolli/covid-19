34f8b69ab83502fac82ec938e28527ed4b0adff5
Matheus Richard Chignolli <matheuschignolli@gmail.com>
2020-07-20 15:25:24 -0300

    Menu Style

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,10 +6,13 @@ import './styles.sass';
 const Navbar = () => {
     return (
         <nav>
-            <ul>
-                <li><Link to='/'>Brasil</Link></li>
-                <li><Link to='/world'>Mundo</Link></li>
-            </ul>
+            <h2>Relatório Covid-19</h2>
+            <div className="top-menu">
+                <ul>
+                    <li><Link to='/'>Brasil</Link></li>
+                    <li><Link to='/world'>Mundo</Link></li>
+                </ul>
+            </div>
         </nav>
     )
 }
